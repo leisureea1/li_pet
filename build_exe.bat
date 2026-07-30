931f80da778a80d091f06123ef8609dac6c8f979
leisureea1 <486261950@qq.com>
2026-07-31 02:46:03 +0800

    Fix Pyinstaller missing Paddlex OCR metadata

--- build_exe.bat
+++ build_exe.bat
@@ -1,4 +1,4 @@
 @echo off
 echo Building Python executable...
-pyinstaller --noconsole --onefile --hidden-import edge_tts --hidden-import pygame --hidden-import pkg_resources.py2_warn --hidden-import pycaw --hidden-import comtypes --hidden-import pandas --hidden-import openpyxl --hidden-import xlrd --add-data "character_fullbody.png;." --add-data "pat_hand_nobg.png;." --collect-data Cython --collect-all paddle --collect-all paddleocr pet.py
+pyinstaller --noconsole --onefile --hidden-import edge_tts --hidden-import pygame --hidden-import pkg_resources.py2_warn --hidden-import pycaw --hidden-import comtypes --hidden-import pandas --hidden-import openpyxl --hidden-import xlrd --add-data "character_fullbody.png;." --add-data "pat_hand_nobg.png;." --collect-data Cython --collect-all paddle --collect-all paddleocr --copy-metadata paddlex --copy-metadata paddlepaddle --copy-metadata paddleocr pet.py
 pause
